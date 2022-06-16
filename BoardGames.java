bddeddb6bb4621c4197c9c8bd64d1a5a55286cb8
bjon1 <bcjon3@gmail.com>
2022-06-15 22:20:56 -0400

    Add TicTacToe class

--- BoardGames.java
+++ BoardGames.java
@@ -8,12 +8,17 @@ public abstract class BoardGames implements ActionListener {
     private JPanel button_panel;
     protected JLabel textfield;
     protected JButton[] buttons; 
+    protected String game;
     
     //Random random = new Random();
     //Within the frame will be title_panel, button_panel, and within title_panel will be textfield
     public BoardGames(int boardSize){
+        frame = new JFrame();
+        title_panel = new JPanel();
+        button_panel = new JPanel();
+        textfield = new JLabel();
+        buttons = new JButton[boardSize*boardSize];
 
-        buttons = new JButton[boardSize];
         //initialize the frame
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800,800);
@@ -23,7 +28,7 @@ public abstract class BoardGames implements ActionListener {
         //initialize the textfield
         textfield.setBackground(new Color(25, 25, 25));
         textfield.setForeground(new Color(25, 255, 0));
-        textfield.setFont(new Font("Ink Free", Font.BOLD, 80));
+        textfield.setFont(new Font("MV Boli", Font.BOLD, 80));
         textfield.setHorizontalAlignment(JLabel.CENTER);
         //textfield.setText("Tic-Tac-Toe");
         textfield.setOpaque(true);
@@ -32,12 +37,14 @@ public abstract class BoardGames implements ActionListener {
         title_panel.setBounds(0,0, 800, 100);
         //initialize button_panel
         button_panel.setLayout(new GridLayout(boardSize,boardSize));
-        button_panel.setBackground(new Color(150, 150, 150)); //drastically different colors to make sure it is added
+        button_panel.setBackground(new Color(25, 25, 25)); //drastically different colors to make sure it is added
 
-        for(int i = 0; i < boardSize*boardSize; i++){
+        for(int i = 0; i < boardSize * boardSize; i++){
             buttons[i] = new JButton();
             button_panel.add(buttons[i]); //you have to add buttons to the button_panel
             buttons[i].setFont(new Font("MV Boli", Font.BOLD, 120));
+            buttons[i].setBackground(Color.BLACK);
+            //buttons[i].setForeground(Color.GRAY);
             buttons[i].setFocusable(false);
             buttons[i].addActionListener(this);
         }
@@ -49,36 +56,10 @@ public abstract class BoardGames implements ActionListener {
         //BorderLayout.NORTH sets the title_panel's position inside Jframe object frame
     }
 
-    public BoardGames(){ //there should be 9 buttons by default; each representing a space of a TicTacToe board
-        this(new TicTacToe(true), 3);
+    public BoardGames(){ //there should be 9 buttons by default;
+        this(3);
     }
 
-        @Override
-    public void actionPerformed(ActionEvent e) {
-        for(int i = 0; i < buttons.length; i++){ //check each of our 9 buttons
-            if(e.getSource() == buttons[i]){ //if the action event was performed on button[i]...
-                if(ttt.getTurn()){ //if it is player1's turn, check to see if there is already text assigned to this button
-                    if(buttons[i].getText() == ""){ //if there is no text on the button...
-                        buttons[i].setForeground(new Color(255,0,0)); //change the foreground
-                        buttons[i].setText("X"); //put an X
-                        ttt.setTurn(false); //make it player2's turn
-                        textfield.setText("O turn"); //change the text to O-turn
-                        //check();
-                    } 
-                } else {
-                    if(buttons[i].getText() == ""){ //if there is no text on the button...
-                        buttons[i].setForeground(new Color(0,0,255)); //change the foreground
-                        buttons[i].setText("O"); //put an O
-                        ttt.setTurn(true); //make it player1's turn
-                        textfield.setText("X turn"); //change the text to O-turn
-                        //check();
-                    }                 
-                }
-            }
-        }
-
-    }
-    
-
+    public abstract void actionPerformed(ActionEvent e); 
 
 }
---
+++ TicTacToe.java
@@ -0,0 +1,118 @@
+import java.awt.*;
+import java.awt.event.*;
+
+public class TicTacToe extends BoardGames{ 
+
+    private char[] movesData = new char[9];
+    private boolean player1_turn;
+    private int numOfMoves;
+
+    public TicTacToe(){
+        this(true);
+    }
+
+    public TicTacToe(boolean p){
+        super(3);
+        game = "Tic-Tac-Toe";
+        numOfMoves = 0;
+        textfield.setText(game);
+        this.player1_turn = p;  
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        for(int i = 0; i < buttons.length; i++){ //check each of our 9 buttons
+            if(e.getSource() == buttons[i]){ //if the action event was performed on button[i]...
+                if(player1_turn){ //if it is player1's turn, check to see if there is already text assigned to this button
+                    if(buttons[i].getText() == "" && movesData[i] == 0){ 
+                        updateMovesData(i, 'X');
+                        buttons[i].setForeground(new Color(255,0,0)); //change the foreground
+                        setTurn(false); //make it player1's turn
+                        checkWin("X wins");
+                    } 
+                } else {
+                    if(buttons[i].getText() == ""){ //if there is no text on the button...
+                        updateMovesData(i, 'O');
+                        buttons[i].setForeground(new Color(0,0,255)); //change the foreground
+                        setTurn(true); //make it player2's turn
+                        checkWin("O wins");
+                    }                 
+                }
+            }
+        }
+
+    }
+    
+    public void updateMovesData(int i, char c){ 
+        movesData[i] = c;
+        numOfMoves++;
+        if(c == 'X'){
+            buttons[i].setText("X"); //put an X
+        } else {
+            buttons[i].setText("O"); //put an O
+        }
+    }
+
+    public boolean getTurn(){
+        return player1_turn;
+    }
+
+    public void setTurn(boolean val){
+        if(val){
+            textfield.setText("X turn"); //change the text to X-turn
+        } else {
+            textfield.setText("O turn"); //change the text to O-turn
+        }
+        player1_turn = val;
+    }
+
+    public void checkWin(String winner){
+        if(numOfMoves == 9){
+            closeButtons();
+            textfield.setText("Draw!");
+        }
+
+        char sym = winner.charAt(0);
+
+        if(movesData[0] == sym && movesData[1] == sym && movesData[2] == sym){
+            wins(0, 1, 2, winner);
+        } else if(movesData[3] == sym && movesData[4] == sym && movesData[5] == sym){
+            wins(3, 4, 5, winner);
+        } else if(movesData[6] == sym && movesData[7] == sym && movesData[8] == sym){
+            wins(6, 7, 8, winner);
+        } else if(movesData[0] == sym && movesData[3] == sym && movesData[6] == sym){
+            wins(0, 3, 6, winner);
+        } else if(movesData[1] == sym && movesData[4] == sym && movesData[7] == sym){
+            wins(1, 4, 7, winner);
+        } else if(movesData[2] == sym && movesData[5] == sym && movesData[8] == sym){
+            wins(2, 5, 8, winner);
+        } else if(movesData[0] == sym && movesData[4] == sym && movesData[8] == sym){
+            wins(0, 4, 8, winner);
+        } else if(movesData[2] == sym && movesData[4] == sym && movesData[6] == sym){
+            wins(2, 4, 6, winner);
+        } 
+        numOfMoves = 0;
+    }
+
+    public void wins(int a, int b, int c, String winner){
+        if(winner.equals("X wins")){
+            buttons[a].setBackground(Color.GREEN);
+            buttons[b].setBackground(Color.GREEN);
+            buttons[c].setBackground(Color.GREEN);
+            textfield.setText(winner);
+        } else {
+            buttons[a].setBackground(Color.GREEN);
+            buttons[b].setBackground(Color.GREEN);
+            buttons[c].setBackground(Color.GREEN);
+            textfield.setText(winner);
+        }
+        closeButtons();
+    }
+
+    public void closeButtons(){
+        for(int i = 0; i < 9; i++){
+            buttons[i].setEnabled(false);
+        }
+    }
+    
+}
